acee4d7e673dfffcccc713792379241c1221c420
Skelde <thomas.feuilletin1@gmail.com>
2022-03-18 09:34:59 +0100

    Alert test and other

--- src/main/scala/tutorial/webapp/DialAndAlert.scala
+++ src/main/scala/tutorial/webapp/DialAndAlert.scala
@@ -11,11 +11,43 @@ import scalafx.scene.control._
 object alert1 extends JFXApp3{
     override def start() : Unit = {
         stage = new JFXApp3.PrimaryStage{
+
+            //Simple alert
             new Alert(AlertType.Information){
                 initOwner(stage)
                 headerText = "Je suis une alert de dialogue tmtc"
                 contentText = "J'ai un grand message pour toi !"
             }.showAndWait()
+
+            new Alert(AlertType.Warning){
+                initOwner(stage)
+                title = "CHAT"
+                headerText = "Ah"
+                contentText = "RAAAH"
+            }.showAndWait()
+
+            new Alert(AlertType.Error){
+                initOwner(stage)
+                title = "Error"
+                headerText = "Error 404"
+                contentText = "Brain Not Found"
+            }.showAndWait()
+
+            //Confirmation dialogue
+
+            val alert = new Alert(AlertType.Confirmation){
+                initOwner(stage)
+                title = "Confirme Dialogue"
+                headerText = "Are you sure ?"
+                contentText = "Realy sure ?"
+            }
+
+            val resultat = alert.showAndWait()
+
+            resultat match {
+                case Some(ButtonType.OK) => println("oh ok")
+                case _ => println("Let's go bae")
+            }
         }
     }
 }
